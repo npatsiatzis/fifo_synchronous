31737141f5679396334f7ea44345f2db91d2cbf3
Nikos Patsiatzis <n.patsiatzis@gmail.com>
2022-12-13 09:32:54 +0200

    initial commit for VHDL synchronous fifo

---
+++ synchronous_fifo.vhd
@@ -0,0 +1,105 @@
+--Dual clock FIFO for communicating data between two synchronous clock domains,
+--the write and read domain.
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity synchronous_fifo is
+	generic (
+			g_width : natural :=8;
+			g_depth : natural :=32);
+	port (
+			i_clk_wr : in std_ulogic;
+			i_rst_wr : in std_ulogic;
+			i_data : in std_ulogic_vector(g_width -1 downto 0);
+			i_wr : in std_ulogic;
+
+			i_clk_rd : in std_ulogic;
+			i_rst_rd : in std_ulogic;
+			i_rd : in std_ulogic;
+
+			o_data : out std_ulogic_vector(g_width -1 downto 0);
+			o_overflow : out std_ulogic;
+			o_underflow : out std_ulogic;
+			o_full : out std_ulogic;
+			o_empty : out std_ulogic
+		);
+end synchronous_fifo;
+
+architecture arch of synchronous_fifo is
+	constant ADDR_DEPTH :natural := natural(ceil(log2(real(g_depth))));	--parameter of log2 is real valued
+
+	signal r_fill : unsigned(ADDR_DEPTH downto 0);
+	signal r_addr_w : unsigned(ADDR_DEPTH downto 0);
+	alias  w_addr_w : unsigned(ADDR_DEPTH -1 downto 0) is r_addr_w(ADDR_DEPTH-1 downto 0);
+	signal r_addr_r : unsigned(ADDR_DEPTH downto 0);
+	alias w_addr_r : unsigned(ADDR_DEPTH-1 downto 0) is r_addr_r(ADDR_DEPTH-1 downto 0);
+
+	type fifo_mem is array(0 to g_depth-1) of std_ulogic_vector(g_width -1 downto 0);
+	signal mem : fifo_mem;
+begin
+
+	--FIFO write domain
+	fifo_write : process(i_clk_wr)
+	begin
+		if(rising_edge(i_clk_wr)) then
+			if(i_rst_wr = '1') then
+				r_addr_w <= (others => '0');
+			else
+				if(i_wr = '1' and o_full = '0') then
+					mem(to_integer(w_addr_w)) <= i_data;
+					r_addr_w <= r_addr_w + 1;
+				end if;
+			end if;
+		end if;
+	end process; -- fifo_write
+
+	--FIFO read domain
+	fifo_read : process(i_clk_rd)
+	begin
+		if(rising_edge(i_clk_rd)) then
+			if(i_rst_rd = '1') then
+				r_addr_r <= (others => '0');
+				o_data <= (others => '0');
+			else
+				if(i_rd = '1' and  o_empty = '0')then
+					o_data <= mem(to_integer(w_addr_r));
+					r_addr_r <= r_addr_r +1;
+				end if;
+			end if;
+		end if;
+	end process; -- fifo_read
+
+	--fifo oveflow/underflow detection
+	overflow_underflow : process(all)
+	begin
+		if(o_full and i_wr) then
+			o_overflow <= '1';
+		else
+			o_overflow <= '0';
+		end if;
+		if(o_empty and i_rd) then
+			o_underflow <= '1';
+		else
+			o_underflow <= '0';
+		end if;
+	end process; -- overflow_underflow
+
+	control_flow : process(all) 
+	begin
+		r_fill <= r_addr_w - r_addr_r;
+		if(r_fill = 0) then
+			o_empty <= '1';
+		else
+			o_empty <= '0';	
+		end if;
+
+		if(r_fill = g_depth) then
+			o_full <= '1';
+		else
+			o_full <= '0';
+		end if;
+	end process; -- control_flow
+end arch;
---
+++ testbench.py
@@ -0,0 +1,119 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer,RisingEdge,FallingEdge,ClockCycles
+from cocotb.result import TestFailure
+import random
+from cocotb_coverage.coverage import CoverCross,CoverPoint,coverage_db
+
+covered_number = []
+g_width = int(cocotb.top.g_width)
+g_depth = int(cocotb.top.g_depth)
+
+full = False
+empty = False
+overflow = False
+underflow = False
+read_when_empty = False
+write_when_full = False
+
+# #Callback functions to capture the bin content showing
+def notify_full():
+	global full
+	full = True
+
+def notify_empty():
+	global empty
+	empty = True
+
+def notify_overflow():
+	global overflow
+	overflow = True
+
+def notify_underflow():
+	global underflow
+	underflow = True
+
+
+# at_least = value is superfluous, just shows how you can determine the amount of times that
+# a bin must be hit to considered covered
+@CoverPoint("top.wr",xf = lambda x : x.i_wr.value, bins = [True,False], at_least=1)
+@CoverPoint("top.rd",xf = lambda x : x.i_rd.value, bins = [True,False],at_least=1)
+@CoverPoint("top.full",xf = lambda x : x.o_full.value,bins = [True,False],at_least=1)
+@CoverPoint("top.empty",xf = lambda x : x.o_empty.value,bins = [True,False],at_least=1)
+@CoverPoint("top.overflow",xf = lambda x : x.o_overflow.value,bins = [True,False],at_least=1)
+@CoverPoint("top.underflow",xf = lambda x : x.o_underflow.value,bins = [True,False],at_least=1)
+def number_cover(dut):
+	pass
+
+async def reset(dut,cycles=1):
+	dut.i_rst_wr.value = 1
+	dut.i_rst_rd.value = 1
+
+	dut.i_wr.value = 0 
+	dut.i_rd.value = 0
+	dut.i_data.value = 0
+
+	await ClockCycles(dut.i_clk_wr,cycles)
+	dut.i_rst_wr.value = 0
+	dut.i_rst_rd.value = 0
+	await RisingEdge(dut.i_clk_wr)
+	dut._log.info("the core was reset")
+
+@cocotb.test()
+async def test(dut):
+	"""Check results and coverage for dual clock synchronous fifo"""
+
+	cocotb.start_soon(Clock(dut.i_clk_wr, 10, units="ns").start())
+	cocotb.start_soon(Clock(dut.i_clk_rd, 10, units="ns").start())
+	await reset(dut,5)	
+	
+	is_full = False
+	is_empty = True
+
+	fifo = []
+	wr_pointer = 0 
+	rd_pointer = 0
+
+	read_element = 0
+
+	while ((full and empty and overflow and underflow) != True):
+		wr = random.randint(0,1)
+		rd = random.randint(0,1)
+		data = random.randint(0,2**g_width-1)
+
+		if((wr == 1 and is_full == False)):
+			fifo.append(data)
+		if((rd == 1 and is_empty == False)):
+			read_element = fifo[0]
+			fifo.pop(0)
+		
+		if((wr == 1 and is_full == False)):
+			wr_pointer = (wr_pointer+1) % g_depth
+
+		if((rd == 1 and is_empty == False)):
+			rd_pointer = (rd_pointer+1) % g_depth
+
+		if(wr_pointer - rd_pointer == g_depth-1):
+			is_full = True
+		else:
+			is_full = False
+
+		if(wr_pointer - rd_pointer == 0):
+			is_empty = True
+		else:
+			is_empty = False
+
+		
+		dut.i_wr.value = wr
+		dut.i_data.value = data
+		dut.i_rd.value = rd
+
+		await RisingEdge(dut.i_clk_wr)
+		await FallingEdge(dut.i_clk_wr)
+		assert not (read_element != int(dut.o_data.value)),"Different expected to actual read data"
+
+		coverage_db["top.full"].add_bins_callback(notify_full, True)
+		coverage_db["top.empty"].add_bins_callback(notify_empty, True)
+		coverage_db["top.overflow"].add_bins_callback(notify_overflow, True)
+		coverage_db["top.underflow"].add_bins_callback(notify_underflow, True)
+		number_cover(dut)
