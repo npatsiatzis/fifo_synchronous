0ad2ae7841f29c247143592b4676855d71a6f5e8
Nikos Patsiatzis <n.patsiatzis@gmail.com>
2023-04-25 19:55:09 +0300

    added comments explaining the operation of the module

--- rtl/synchronous_fifo.vhd
+++ rtl/synchronous_fifo.vhd
@@ -1,6 +1,10 @@
 --Dual clock FIFO for communicating data between two syncrhonous clock domains,
 --the write and read domain.
 
+--The FIFO operation rests upon 2 ready/valid handshakes, 1 for the write "domain" 
+-- and 1 for the read "domain". Each of the handshakes consist of a Control Path 
+--(trivial FSM) and a Data Path (performs the actions (push/pop) on the buffer).
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -57,6 +61,15 @@ begin
 	end process; -- veirification_util
 
 	--FIFO write domain
+
+	--FIFO write ready/valid handshake, also called the push/full handshake
+	--data flow control signals:
+	--i_wr/valid <-> !o_full/ready
+
+	--Write domain handhsake Control Path
+	--2 data flow control signals (ready/valid) make up 4 possible states for the connection
+	--Trivial FSM with 3 states collapsed in 1 (00,01,10) and the other state (11)
+	--being the state where the transaction occurs.
 	fifo_write : process(i_clk_wr)
 	begin
 		if(rising_edge(i_clk_wr)) then
@@ -76,6 +89,13 @@ begin
 	end process; -- fifo_write
 
 	--FIFO read domain
+
+	--FIFO read ready/valid handshake, also called the pop/empty handshake
+	--data flow control signals:
+	--i_rd/valid <-> !o_empty/ready
+
+	--Read domain Control Path
+	--FSM strucure analogous to the one in the Write domain
 	fifo_read : process(i_clk_rd)
 	begin
 		if(rising_edge(i_clk_rd)) then
