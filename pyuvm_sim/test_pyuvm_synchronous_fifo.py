a8d9a701387d938c196d2c8275dce8453be4eab9
Nikos Patsiatzis <n.patsiatzis@gmail.com>
2023-08-10 07:36:43 +0300

    corrected inconcistencies between the cocotb & pyuvm tests with their pytest counterparts

--- pyuvm_sim/test_pyuvm_synchronous_fifo.py
+++ pyuvm_sim/test_pyuvm_synchronous_fifo.py
@@ -19,7 +19,7 @@ def test_fifo(g_width,g_depth):
     module = "tb"
     toplevel = "synchronous_fifo"   
     vhdl_sources = [
-        os.path.join(rtl_dir, "../rtl/synchronous_fifo.vhd"),
+        os.path.join(rtl_dir, "../rtl/VHDL/synchronous_fifo.vhd"),
         ]
 
     parameter = {}
